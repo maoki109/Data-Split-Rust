e09b510a76f0f64af64e2704a70844b1622ef59a
Mari Aoki <mariaoki@pop-os.localdomain>
2023-10-06 12:49:02 -0600

    Creation of new directories now as a function

--- src/main.rs
+++ src/main.rs
@@ -3,16 +3,13 @@ use std::path::Path;
 use rand::thread_rng;
 use rand::seq::SliceRandom;
 
-// struct instance {
-//     image: String,
-//     label: String,
-// }
-
 fn main() {
     let args: Vec<String> = env::args().collect();
 
     let training_ratio = &args[1];
     let file_path = &args[2];
+
+    print_type_of(&file_path);
 
     println!("{} is how much we want in the training set, rest in validation set. ", training_ratio);
     println!("From the directory {}", file_path);
@@ -63,55 +60,47 @@ fn main() {
     println!("image indices: {:?}", split_ind[0]);
     println!("label indices: {:?}", split_ind[1]);
 
-    // make into a function
-    // for new training directory
-    let new_train_image_path = file_path.strip_suffix("train").unwrap().to_string()+"new_train/images";
-    let new_train_label_path = file_path.strip_suffix("train").unwrap().to_string()+"new_train/labels";
+    // Creating new train and validation directory
+    new_directory("train", file_path);
+    new_directory("val", file_path);
 
-    println!("New images path: {:?}", new_train_image_path);
-    println!("New labels path: {:?}", new_train_label_path);
+    // Copy training image and label files into new directories
+    // for n in split_ind[0] {
+    //     // println!("{:?}", image_vector[*n]);
+    //     // let img_name = format!("/images/{:?}", image_vector[*n]);
+    //     // let label_name = format!("/labels/{:?}", label_vector[*n]);
+    //     let img_name = "/images/".to_owned()+&image_vector[*n];
+    //     let label_name = "/labels/".to_owned()+&label_vector[*n];
+    //     let old_image_path = file_path.to_string()+&img_name;
+    //     let old_label_path = file_path.to_string()+&label_name;
+    //     // let testname = new_train_image_path.clone()+&image_vector[*n];
+    //     println!("Old image path: {:?}", old_image_path);
+    //     println!("New image path: {:?}", new_train_image_path.clone()+&image_vector[*n]);
+    //     fs::copy(old_image_path, new_train_image_path.clone()+&image_vector[*n]);
+    //     fs::copy(old_label_path, new_train_label_path.clone()+&label_vector[*n]);
+    // }
 
-    match Path::new(&new_train_image_path).try_exists() {
-        Ok(false) => {
-            fs::create_dir_all(new_train_image_path.clone());
-            println!("New directory for images created at {:?}", new_train_image_path);
-        },
-        Ok(true) => println!("Directory already exists"),
-        Err(_) => println!("Error!"),
+    // Copy validation image and label filesinto new directories
+}
+
+fn new_directory(split_type: &str, file_path: &str) {
+    // println!("Split type: {:?}", split_type);
+    // println!("File type: {:?}", file_type);
+    let file_types = ["images", "labels"];
+
+    for file_type in file_types {
+        let new_path = file_path.strip_suffix("train").unwrap().to_string()+"new_"+&split_type+"/"+&file_type+"/";
+        match Path::new(&new_path).try_exists() {
+            Ok(false) => {
+                fs::create_dir_all(new_path.clone());
+                println!("New directory created at {:?}", new_path);
+            },
+            Ok(true) => println!("Directory already exists"),
+            Err(_) => println!("Error!"),
+        }
     }
+}
 
-    match Path::new(&new_train_label_path).try_exists() {
-        Ok(false) => {
-            fs::create_dir_all(new_train_label_path.clone());
-            println!("New directory for labels created at {:?}", new_train_label_path);
-        },
-        Ok(true) => println!("Directory already exists"),
-        Err(_) => println!("Error!"),
-    }
-
-    // for new validation directory
-    let new_val_image_path = file_path.strip_suffix("train").unwrap().to_string()+"new_val/images";
-    let new_val_label_path = file_path.strip_suffix("train").unwrap().to_string()+"new_val/labels";
-
-    println!("New val images path: {:?}", new_val_image_path);
-    println!("New val labels path: {:?}", new_val_label_path);
-
-    match Path::new(&new_val_image_path).try_exists() {
-        Ok(false) => {
-            fs::create_dir_all(new_val_image_path.clone());
-            println!("New directory for val images created at {:?}", new_val_image_path);
-        },
-        Ok(true) => println!("Directory already exists"),
-        Err(_) => println!("Error!"),
-    }
-
-    match Path::new(&new_val_label_path).try_exists() {
-        Ok(false) => {
-            fs::create_dir_all(new_val_label_path.clone());
-            println!("New directory for val labels created at {:?}", new_val_label_path);
-        },
-        Ok(true) => println!("Directory already exists"),
-        Err(_) => println!("Error!"),
-    }
-
+fn print_type_of<T>(_: &T) {
+    println!("{}", std::any::type_name::<T>())
 }
